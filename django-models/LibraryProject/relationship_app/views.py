f451dc98d0ab7d198c7c5c1aef9815ae65fda7f5
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2025-11-09 20:26:02 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,10 @@ from .models import Book, Library
 from django.contrib.auth.views import LoginView, LogoutView 
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 
 # ✅ Function-Based View: List all books
