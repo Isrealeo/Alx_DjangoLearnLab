8c13625a0986215db5f8955ac06e890e7ad09398
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2025-11-09 20:29:01 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,9 +14,9 @@ from .views import (
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # FBV
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
-    path('register/', UserRegisterView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
